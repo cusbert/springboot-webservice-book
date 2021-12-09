0d03d23caa947e03e4d3fb9274ad10b8574d5e5f
cusbert <mindychoi20@gmail.com>
2021-12-09 23:52:22 +0900

    first commit

---
+++ src/test/java/com/practice/book/SpringbootWebserviceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
