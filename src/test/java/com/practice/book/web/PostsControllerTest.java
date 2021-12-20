f0a2899f5cedefc094ef5f06c9eacf8abebeb2e1
cusbert <mindychoi20@gmail.com>
2021-12-21 00:14:44 +0900

    test: #1 Post 수정, 상세 TC 추가

--- src/test/java/com/practice/book/web/PostsControllerTest.java
+++ src/test/java/com/practice/book/web/PostsControllerTest.java
@@ -3,6 +3,7 @@ package com.practice.book.web;
 import com.practice.book.domain.posts.Posts;
 import com.practice.book.domain.posts.PostsRepository;
 import com.practice.book.web.dto.PostsSaveRequestDto;
+import com.practice.book.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,6 +26,12 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsControllerTest {
 
+    public static final String TITLE = "TITLE";
+    public static final String CONTENT = "CONTENT";
+    public static final String AUTHOR = "AUTHOR";
+
+    public static final String EXPECTED_TITLE = "TITLE2";
+    public static final String EXPECTED_CONTENT = "CONTENT2";
     @LocalServerPort
     private int port;
 
@@ -41,14 +50,10 @@ public class PostsControllerTest {
     public void Post_등록_성공() {
 
         // given
-        String title = "TITLE";
-        String content = "CONTENT";
-        String author = "AUTHOR";
-
         PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
-                .title(title)
-                .content(content)
-                .author(author)
+                .title(TITLE)
+                .content(CONTENT)
+                .author(AUTHOR)
                 .build();
 
         String url = "http://localhost:" + port + "/api/v1/posts";
@@ -61,8 +66,65 @@ public class PostsControllerTest {
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(title);
-        assertThat(all.get(0).getContent()).isEqualTo(content);
+        assertThat(all.get(0).getTitle()).isEqualTo(TITLE);
+        assertThat(all.get(0).getContent()).isEqualTo(CONTENT);
 
+    }
+
+    @Test
+    public void Post_수정_성공() throws Exception {
+
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .author(AUTHOR)
+                .build());
+
+        Long updatedId = savedPosts.getId();
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(EXPECTED_TITLE)
+                .content(EXPECTED_CONTENT)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updatedId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(EXPECTED_TITLE);
+        assertThat(all.get(0).getContent()).isEqualTo(EXPECTED_CONTENT);
+    }
+
+    @Test
+    public void Post_상세정보_조회_성공() throws Exception {
+
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .author(AUTHOR)
+                .build());
+
+        Long updatedId = savedPosts.getId();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updatedId;
+
+        // when
+        ResponseEntity<String> responseEntity =  restTemplate.getForEntity(url, String.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(TITLE);
+        assertThat(all.get(0).getContent()).isEqualTo(CONTENT);
     }
 }
