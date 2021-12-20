f84c0d80a3b279f39151a1e9b8661f7c2a3e2ee5
cusbert <mindychoi20@gmail.com>
2021-12-20 23:56:04 +0900

    feat: #1 Post 수정, 상세 조회 기능 추가

--- src/main/java/com/practice/book/service/PostsService.java
+++ src/main/java/com/practice/book/service/PostsService.java
@@ -1,11 +1,14 @@
 package com.practice.book.service;
 
+import com.practice.book.domain.posts.Posts;
 import com.practice.book.domain.posts.PostsRepository;
+import com.practice.book.web.dto.PostsResponseDto;
 import com.practice.book.web.dto.PostsSaveRequestDto;
+import com.practice.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +19,21 @@ public class PostsService {
     @Transactional
     public long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/practice/book/web/PostsController.java
+++ src/main/java/com/practice/book/web/PostsController.java
@@ -2,6 +2,8 @@ package com.practice.book.web;
 
 import com.practice.book.service.PostsService;
 import com.practice.book.web.dto.PostsSaveRequestDto;
+import com.practice.book.web.dto.PostsResponseDto;
+import com.practice.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,5 +16,16 @@ public class PostsController {
     @PostMapping("/api/v1/posts")
     public long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
