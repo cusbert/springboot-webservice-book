4e73f7cf03563e781557944182b9e1ca4b17a67c
cusbert <mindychoi20@gmail.com>
2021-12-23 21:54:11 +0900

    feat: #1 JPA Auditing 으로 생성/수정 시간 자동화 기능 추가

--- src/main/java/com/practice/book/SpringbootWebserviceBookApplication.java
+++ src/main/java/com/practice/book/SpringbootWebserviceBookApplication.java
@@ -2,12 +2,14 @@ package com.practice.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootWebserviceBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebserviceBookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebserviceBookApplication.class, args);
+    }
 
 }
