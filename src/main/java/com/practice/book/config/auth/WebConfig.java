0cb43ecfa439e62ea69c17cac5fcdf8c0191225c
cusbert <mindychoi20@gmail.com>
2022-01-01 23:55:38 +0900

    refactor: 어노테이션 기반으로 세션값 가져오도록 수정

---
+++ src/main/java/com/practice/book/config/auth/WebConfig.java
@@ -0,0 +1,20 @@
+package com.practice.book.config.auth;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        // HandlerMethodArgumentResolver 는 항상 WebMvcConfigurer 의 addArgumentResolvers를 통해 추가되어야 한다
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/practice/book/web/IndexController.java
+++ src/main/java/com/practice/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.practice.book.web;
 
+import com.practice.book.config.auth.LoginUser;
 import com.practice.book.config.auth.dto.SessionUser;
 import com.practice.book.service.PostsService;
 import com.practice.book.web.dto.PostsResponseDto;
@@ -19,9 +20,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        // @LoginUser SessionUser user 로 개선
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
