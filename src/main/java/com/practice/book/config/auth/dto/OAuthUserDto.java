097f55edbbd91eb81e603ec7e67e9cbe5927329a
cusbert <mindychoi20@gmail.com>
2022-01-02 23:09:54 +0900

    feat: naver 로그인 기능 추가

--- src/main/java/com/practice/book/config/auth/dto/OAuthUserDto.java
+++ src/main/java/com/practice/book/config/auth/dto/OAuthUserDto.java
@@ -17,7 +17,8 @@ public class OAuthUserDto {
     private String picture;
 
     @Builder
-    public OAuthUserDto(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+    public OAuthUserDto(Map<String, Object> attributes, String nameAttributeKey, String name,
+        String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -26,28 +27,48 @@ public class OAuthUserDto {
     }
 
 
-    public static OAuthUserDto of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+    public static OAuthUserDto of(String registrationId, String userNameAttributeName,
+        Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    private static OAuthUserDto ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthUserDto ofNaver(String userNameAttributeName,
+        Map<String, Object> attributes) {
+
+        Map<String,  Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthUserDto.builder()
+            .name((String) response.get("name"))
+            .email((String) response.get("email"))
+            .picture((String) response.get("profile_image"))
+            .attributes(response)
+            .nameAttributeKey(userNameAttributeName)
+            .build();
+    }
+
+    private static OAuthUserDto ofGoogle(String userNameAttributeName,
+        Map<String, Object> attributes) {
 
         return OAuthUserDto.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("picture"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
-                .build();
+            .name((String) attributes.get("name"))
+            .email((String) attributes.get("email"))
+            .picture((String) attributes.get("picture"))
+            .attributes(attributes)
+            .nameAttributeKey(userNameAttributeName)
+            .build();
     }
+
 
     // OAuthUserDto 에서 엔티티를 생성하는 시점은 가입할 때이므로 Guest 권한 디폴트
     public User toEntity() {
         return User.builder()
-                .name(name)
-                .email(email)
-                .picture(picture)
-                .role(Role.GUEST)
-                .build();
+            .name(name)
+            .email(email)
+            .picture(picture)
+            .role(Role.GUEST)
+            .build();
     }
 }
